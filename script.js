c4293ab7ecdeabab68a3f52d5c54dbc7407f056b
moonpeach909 <zadiecalveryn@gmail.com>
2026-03-08 04:08:55 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const cards=document.querySelectorAll(".card")
+
+cards.forEach(card=>{
+card.addEventListener("mouseover",()=>{
+card.style.transform="scale(1.05)"
+})
+card.addEventListener("mouseout",()=>{
+card.style.transform="scale(1)"
+})
+})
